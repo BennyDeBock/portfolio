8a729d73811fc2cc4d6635bcd9196ec3f9609c05
De Bock Benny <bdebock@protonmail.com>
2023-07-20 17:32:55 +0200

    Add simple layout

---
+++ layouts/default.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <nav>
+            <NuxtLink class="link" to="/">Home</NuxtLink>
+            <NuxtLink class="link" to="/blog">Blog</NuxtLink>
+            <NuxtLink class="link" to="/release-notes">Release notes</NuxtLink>
+        </nav>
+        <div>
+            <slot />
+        </div>
+    </div>
+</template>
+
+<style>
+.link {
+    margin-right: 1rem;
+}
+</style>
--- pages/release-notes/index.vue
+++ pages/release-notes/index.vue
@@ -1,3 +1,4 @@
 <template>
+    <p>0.0.2 - Add simple layout with navigation</p>
     <p>0.0.1 - First page has been added</p>
 </template>
