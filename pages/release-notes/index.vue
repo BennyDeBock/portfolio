3c9d34b3375b9dace04ebc58caec9ce823fb3707
De Bock Benny <bdebock@protonmail.com>
2023-07-24 22:45:01 +0200

    Edit release notes

--- pages/release-notes/index.vue
+++ pages/release-notes/index.vue
@@ -1,4 +1,5 @@
 <template>
+    <p>0.1.0 - Added footer</p>
     <p>0.0.3 - Add blog structure to content folder</p>
     <p>0.0.2 - Add simple layout with navigation</p>
     <p>0.0.1 - First page has been added</p>
