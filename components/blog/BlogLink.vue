e89402491082f371db2473adb29a30b47ddb9290
De Bock Benny <bdebock@protonmail.com>
2023-07-26 23:03:24 +0200

    Add Bloglink component

---
+++ components/blog/BlogLink.vue
@@ -0,0 +1,28 @@
+<template>
+    <article>
+        <header>
+            <NuxtLink :to="props.path" class="card">
+                <h2>{{ props.title }}</h2>
+                <h3>{{ props.date }}</h3>
+            </NuxtLink>
+        </header>
+        <p>{{ excerpt }}</p>
+        <footer v-if="tags">
+            <span v-for="tag in props.tags" :key="tag">
+                {{ tag }}
+            </span>
+        </footer>
+    </article>
+</template>
+
+<script setup lang="ts">
+interface BlogLinkProps {
+    path: string,
+    title: string,
+    date: string,
+    excerpt: string,
+    tags?: string[],
+}
+
+const props = defineProps<BlogLinkProps>()
+</script>
--- components/content/BlogPosts.vue
+++ components/content/BlogPosts.vue
@@ -1,13 +1,12 @@
 <template>
-  <h1>Blog</h1>
   <ul>
     <li v-for="{ _path: slug, title, date, tags, excerpt } in blogPosts" :key="slug">
-      <NuxtLink :to="slug">
-        <h2>{{ title }}</h2>
-        <h3>{{ date }}</h3>
-        <p>{{ excerpt }}</p>
-        <h4 v-for="tag in tags" :key="tag">{{ tag }}</h4>
-      </NuxtLink>
+      <BlogLink
+        :path="slug"
+        :title="title"
+        :date="date"
+        :excerpt="excerpt"
+        :tags="tags" />
     </li>
   </ul>
 </template>
