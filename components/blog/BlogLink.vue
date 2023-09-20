05d4730bc35ce2d0945756059c207f03c10fff1a
De Bock Benny <bdebock@protonmail.com>
2023-09-20 23:55:38 +0200

    start card layout with bulma

--- components/blog/BlogLink.vue
+++ components/blog/BlogLink.vue
@@ -1,15 +1,17 @@
 <template>
-    <article class="nes-container is-rounded article">
-        <header>
+    <article class="card">
+        <header class="card-header">
             <NuxtLink :to="props.path">
-                <h2>{{ props.title }}</h2>
+                <h2 class="card-header-title">{{ props.title }}</h2>
                 <h3>{{ props.date }}</h3>
             </NuxtLink>
         </header>
-        <p>{{ excerpt }}</p>
-        <footer v-if="tags">
-            <a href="#" v-for="tag in props.tags" :key="tag" class="nes-badge">
-                <span class="is-dark">{{ tag }}</span>
+        <div class="card-content">
+          <p class="content">{{ excerpt }}</p>
+        </div>
+        <footer v-if="tags" class="tags">
+            <a href="#" v-for="tag in props.tags" :key="tag" class="tag is-rounded">
+                <span>{{ tag }}</span>
             </a>
         </footer>
     </article>
@@ -28,37 +30,4 @@ const props = defineProps<BlogLinkProps>()
 </script>
 
 <style scoped>
-/* ===== Article component ===== */
-.nes-container.article {
-  max-width: 90%;
-  margin: 0 0 5px 5%;
-}
-
-.nes-container.article h2 {
-  font-size: 1.0rem;
-}
-
-.nes-container.article h3 {
-  font-size: 0.9rem;
-}
-
-.nes-container.article p {
-  font-size: 0.8rem;
-}
-
-.nes-container.article footer {
-  display: flex;
-  justify-content: left;
-  flex-wrap: wrap;
-}
-
-.nes-container.article .nes-badge {
-  width: unset;
-  margin: .5em .7em;
-}
-
-.nes-container.article footer a span.is-dark:first-child {
-  position: relative;
-  width: fit-content;
-}
 </style>
