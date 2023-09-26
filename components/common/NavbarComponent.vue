fb56c2e572faefc3406e99a3553b13d7e32964ab
De Bock Benny <bdebock@protonmail.com>
2023-09-26 21:20:33 +0200

    start styling navbar

--- components/common/NavbarComponent.vue
+++ components/common/NavbarComponent.vue
@@ -1,8 +1,8 @@
 <template>
-  <nav class="navbar is-primary" role="navigation" aria-label="main navigation">
+  <nav class="navbar" role="navigation" aria-label="main navigation">
     <div class="navbar-brand">
-      <NuxtLink class="navbar-item" to="/">
-        <h1>Benny</h1>
+      <NuxtLink class="navbar-item is-size-2" to="/">
+        Benny
       </NuxtLink>
 
       <a role="button" 
@@ -21,10 +21,10 @@
 
     <div id="navMenu" class="navbar-menu" :class="{'is-active': isActive}">
       <div class="navbar-end">
-        <NuxtLink class="navbar-item" to="/blog">Blog</NuxtLink>
-        <NuxtLink class="navbar-item" to="/cv">CV</NuxtLink>
-        <NuxtLink class="navbar-item" to="/projects">Projects</NuxtLink>
-        <NuxtLink class="navbar-item" to="/release-notes">Release Notes</NuxtLink>
+        <NuxtLink class="navbar-item is-size-4" to="/blog">Blog</NuxtLink>
+        <NuxtLink class="navbar-item is-size-4" to="/cv">CV</NuxtLink>
+        <NuxtLink class="navbar-item is-size-4" to="/projects">Projects</NuxtLink>
+        <NuxtLink class="navbar-item is-size-4" to="/release-notes">Release Notes</NuxtLink>
       </div>
     </div>
   </nav>
@@ -32,16 +32,20 @@
 
 <script setup lang="ts">
 let isActive = ref(false)
-let showNavBar = ref(true)
 </script>
 
 <style scoped>
 .navbar {
   font-family: 'Ubuntu';
+  background-color: var(--primary-colour);
   transition: transform 200ms ease-out;
 }
 
 .navbar.is-transformed {
   transform: translate3d(0, -100%, 0);
+}
+
+.navbar-item {
+  color: var(--text-colour);
 }
 </style>
