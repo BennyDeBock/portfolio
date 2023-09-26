2cf57ba83426d55a8596d8c8552d0c8fc38ddbc0
De Bock Benny <bdebock@protonmail.com>
2023-09-26 20:07:27 +0200

    wrap navbar in header tag and create navbar component

---
+++ components/common/NavbarComponent.vue
@@ -0,0 +1,47 @@
+<template>
+  <nav class="navbar is-primary" role="navigation" aria-label="main navigation">
+    <div class="navbar-brand">
+      <NuxtLink class="navbar-item" to="/">
+        <h1>Benny</h1>
+      </NuxtLink>
+
+      <a role="button" 
+        class="navbar-burger"
+        :class="{'is-active': isActive}"
+        aria-label="menu" 
+        :aria-expanded="isActive"
+        @click="isActive = !isActive"
+        data-target="navMenu"
+      >
+        <span aria-hidden="true"></span>
+        <span aria-hidden="true"></span>
+        <span aria-hidden="true"></span>
+      </a>
+    </div>
+
+    <div id="navMenu" class="navbar-menu" :class="{'is-active': isActive}">
+      <div class="navbar-end">
+        <NuxtLink class="navbar-item" to="/blog">Blog</NuxtLink>
+        <NuxtLink class="navbar-item" to="/cv">CV</NuxtLink>
+        <NuxtLink class="navbar-item" to="/projects">Projects</NuxtLink>
+        <NuxtLink class="navbar-item" to="/release-notes">Release Notes</NuxtLink>
+      </div>
+    </div>
+  </nav>
+</template>
+
+<script setup lang="ts">
+let isActive = ref(false)
+let showNavBar = ref(true)
+</script>
+
+<style scoped>
+.navbar {
+  font-family: 'Ubuntu';
+  transition: transform 200ms ease-out;
+}
+
+.navbar.is-transformed {
+  transform: translate3d(0, -100%, 0);
+}
+</style>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,19 +1,9 @@
 <template>
-    <div>
-        <nav>
-            <NuxtLink class="link" to="/">Home</NuxtLink>
-            <NuxtLink class="link" to="/blog">Blog</NuxtLink>
-            <NuxtLink class="link" to="/release-notes">Release notes</NuxtLink>
-        </nav>
-        <div class="container">
-            <slot />
-        </div>
-    </div>
-    <FooterComponent></FooterComponent>
+  <header>
+    <NavbarComponent></NavbarComponent>
+  </header>
+  <main class="container">
+    <slot />
+  </main>
+  <FooterComponent></FooterComponent>
 </template>
-
-<style>
-.link {
-    margin-right: 1rem;
-}
-</style>
