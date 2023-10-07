c9d42385d6006b9bf101ad140926e089323eb4c1
De Bock Benny <bdebock@protonmail.com>
2023-10-07 16:27:07 +0200

    Change blogpost to have proper formatting

--- components/blog/BlogLink.vue
+++ components/blog/BlogLink.vue
@@ -1,20 +1,26 @@
 <template>
-    <article class="card column">
+    <div class="column">
+        <article class="card">
         <header class="card-header">
-            <NuxtLink :to="props.path">
-                <h2 class="card-header-title">{{ props.title }}</h2>
-                <h3>{{ props.date }}</h3>
+            <NuxtLink :to="props.path" class="card-header-title">
+                <h2 class="title is-5">
+                    {{ props.title }} | <span class="subtitle is-6">{{ props.date }}</span>
+                </h2>
             </NuxtLink>
         </header>
         <div class="card-content">
           <p class="content">{{ excerpt }}</p>
         </div>
-        <footer v-if="tags" class="tags">
-            <a href="#" v-for="tag in props.tags" :key="tag" class="tag is-rounded">
-                <span>{{ tag }}</span>
-            </a>
+        <footer v-if="tags" class="card-footer">
+            <div class="tags my-2">
+                <a href="#" v-for="tag in props.tags" :key="tag" class="tag is-rounded">
+                    <span>{{ tag }}</span>
+                </a>
+            </div>
         </footer>
     </article>
+    </div>
+    
 </template>
 
 <script setup lang="ts">
@@ -30,4 +36,24 @@ const props = defineProps<BlogLinkProps>()
 </script>
 
 <style scoped>
+.card {
+    background-color: var(--background-colour-2);
+    color: var(--text-colour);
+}
+
+.card-header {
+    background-color: var(--primary-colour);
+}
+
+.title {
+    color: var(--text-colour);
+}
+
+.subtitle {
+    color: var(--text-colour-2);
+}
+
+.card-footer {
+    border-top-color: var(--primary-colour);
+}
 </style>
--- pages/blog/index.vue
+++ pages/blog/index.vue
@@ -1,6 +1,19 @@
 <template>
-    <h1>Articles</h1>
-    <section class="columns">
+    <section id="intro" class="section">
+        <h1 class="title is-3">Articles</h1>
+        <p>Sometimes I have thoughts, so I write these down.</p>
+    </section>
+    <section class="columns section">
         <BlogPosts></BlogPosts>
     </section>
 </template>
+
+<style scoped>
+#intro h1 {
+  color: var(--text-colour);
+}
+
+#intro p {
+  color: var(--text-colour-2);
+}
+</style>
