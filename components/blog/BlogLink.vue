4af42f3d4f9b520d050d27d34bdb54a08811087a
De Bock Benny <bdebock@protonmail.com>
2024-01-23 13:13:11 +0100

    Remove code that's been repurposed as utility

--- components/blog/BlogLink.vue
+++ components/blog/BlogLink.vue
@@ -33,17 +33,6 @@ interface BlogLinkProps {
 
 const props = defineProps<BlogLinkProps>()
 
-const toReadableDate = (dateString: string) => {
-  const date = new Date(dateString)
-  const day = date.toLocaleDateString('default', { day: 'numeric'})
-  const dayWithSuffix = 
-    +day > 3 ? `${day}th` :  
-    +day > 2 ? `${day}rd` :
-    +day > 1 ? `${day}nd` : `${day}st` 
-  const month = date.toLocaleDateString('default', { month: 'long' })
-  return `${month} ${dayWithSuffix}, ${date.getFullYear()}`
-}
-
 useHead({
   title: 'Blog'
 })
@@ -65,10 +54,6 @@ useHead({
 
 .title {
   color: var(--text-colour);
-}
-
-.subtitle {
-  color: var(--text-colour-2);
 }
 
 .card-footer {
