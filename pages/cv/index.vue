bb795411e6e994c75b6f609a3cb026cf47eb59e0
De Bock Benny <bdebock@protonmail.com>
2024-02-16 22:59:36 +0100

    Start working on the CV page

--- pages/cv/index.vue
+++ pages/cv/index.vue
@@ -1,11 +1,35 @@
 <template>
-  <div style="height: 100vh">
-    This will probably be filled in at some point...someday...perhaps......
-  </div>
+  <NuxtLayout name="cv">
+    <section class="page" size="A4">
+      test
+    </section>
+  </NuxtLayout>
 </template>
 
 <script setup lang="ts">
+definePageMeta({
+  layout: false
+})
 useHead({
   title: 'CV'
 })
 </script>
+
+<style scoped>
+.page[size="A4"] {
+  background: white;
+  width: 21cm;
+  height: 29.7cm;
+  display: block;
+  margin: 0 auto;
+  margin-bottom: 0.5cm;
+  box-shadow: 0 0 0.5cm rgba(0,0,0,0.5);
+}
+
+@media print {
+  .page[size="A4"] {
+    margin: 0;
+    box-shadow: 0;
+  }
+}
+</style>
