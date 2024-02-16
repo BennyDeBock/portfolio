47cd7f946eb4bc8c4a01026f36773299dcc6f9ae
De Bock Benny <bdebock@protonmail.com>
2024-02-16 22:59:25 +0100

    Create a cv layout page

---
+++ layouts/cv.vue
@@ -0,0 +1,18 @@
+<template>
+  <main class="container">
+    <slot />
+  </main>
+</template>
+
+<style scoped>
+main {
+  background: rgb(204,204,204);
+}
+
+@media print {
+  main {
+    margin: 0;
+    box-shadow: 0;
+  }
+}
+</style>
